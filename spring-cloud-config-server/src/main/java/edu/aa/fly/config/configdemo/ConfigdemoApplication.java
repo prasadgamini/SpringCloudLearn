5e232c1473fa03d7f28b62c474c5010a4a10ce39
Prasad Gamini <>
2018-12-27 17:09:23 -0600

    Renamed to server

--- spring-cloud-config-server/src/main/java/edu/aa/fly/config/configdemo/ConfigdemoApplication.java
+++ spring-cloud-config-server/src/main/java/edu/aa/fly/config/configdemo/ConfigdemoApplication.java
@@ -2,13 +2,15 @@ package edu.aa.fly.config.configdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigdemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigdemoApplication.class, args);
+    }
 
 }
 
