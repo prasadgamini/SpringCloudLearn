0ca1fd13d282f9b194a22c42aac2a6691e84ecb9
Prasad Gamini <>
2018-12-28 15:59:26 -0600

    Adding Demo Eureka Client template

---
+++ demo-eureka-client/src/main/java/edu/aa/fly/demoeurekaclient/DemoEurekaClientApplication.java
@@ -0,0 +1,14 @@
+package edu.aa.fly.demoeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEurekaClientApplication.class, args);
+	}
+
+}
+
