a26f8345653cdf483ca109577eeea47951419e81
Prasad Gamini <>
2018-12-27 18:17:02 -0600

    Added a controller and profiles

--- spring-cloud-config-client/src/main/java/edu/aa/fly/config/controller/HelloController.java
+++ spring-cloud-config-client/src/main/java/edu/aa/fly/config/controller/HelloController.java
@@ -1,4 +1,18 @@
 package edu.aa.fly.config.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @Value("${sample.numberOfLessons}")
+    private Integer numberOfLessons;
+
+    @RequestMapping("/hi")
+    public String sayHi(){
+        System.out.printf("ONV");
+        return "ONV..." + numberOfLessons;
+    }
 }
--- spring-cloud-config-client/src/main/java/edu/aa/fly/config/democonfigclient/DemoConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/edu/aa/fly/config/democonfigclient/DemoConfigClientApplication.java
@@ -1,10 +1,13 @@
 package edu.aa.fly.config.democonfigclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "edu.aa")
 public class DemoConfigClientApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoConfigClientApplication.class, args);
