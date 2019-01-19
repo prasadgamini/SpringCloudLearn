ed8ce487012d29873c38e763a8a83c9d44d49ce7
Prasad Gamini <>
2019-01-19 17:58:38 -0600

    Changing contents

--- spring-cloud-config-client/src/main/java/edu/aa/fly/config/controller/LuckyWordController.java
+++ spring-cloud-config-client/src/main/java/edu/aa/fly/config/controller/LuckyWordController.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@ConfigurationProperties(prefix = "wordConfig")
+@ConfigurationProperties(prefix = "word-config")
 @RestController
 public class LuckyWordController {
 
@@ -14,5 +14,13 @@ public class LuckyWordController {
     @GetMapping("/lucky-word")
     public String getLuckyWord(){
         return String.join(":", preamble, luckyWord);
+    }
+
+    public void setLuckyWord(String luckyWord) {
+        this.luckyWord = luckyWord;
+    }
+
+    public void setPreamble(String preamble) {
+        this.preamble = preamble;
     }
 }
